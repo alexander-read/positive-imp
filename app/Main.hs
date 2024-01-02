e95ca56efdc400b036f2f5397d58f038ec2d6b8a
Alex Read <97128707+alexander-read@users.noreply.github.com>
2024-01-02 21:25:33 +0000

    Add REPL to main

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,29 @@
 module Main where
 
+import System.IO ( stdout, hFlush )
+import Control.Monad ( unless )
+
 import Parser
+import Pretty
+
+-- TODO: use flags to determine infix or prefix parsing
 
 main :: IO ()
 main = do
-    expr <- getLine
-    print $ fst $ (parse parsePrefix expr) !! 0
+    putStrLn "Welcome to the REPL!"
+    putStrLn "To quit, type ':q'"
+    mainAux
+
+mainAux :: IO ()
+mainAux = do
+    expr <- greet
+    unless (expr == ":q") $ pprintInf (eval expr) >> mainAux
+
+greet :: IO String
+greet = do
+    putStr "Implication> "
+    hFlush stdout
+    getLine
+
+eval :: String -> Prop
+eval str = fst $ (parse parsePrefix str) !! 0
